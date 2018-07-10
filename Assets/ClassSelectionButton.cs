ad5675947f4e8b6c3303f0f4ab8c16d8e3381758
Joel <disturbed_4u2001@yahoo.com>
2018-07-09 22:56:13 -0400

    Assets

---
+++ Assets/ClassSelectionButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassSelectionButton : MonoBehaviour 
+{
+    public CharacterData character { get; set; }
+
+    // Use this for initialization
+    void Start () 
+    {
+    
+    }
+
+    void onSelected()
+    {
+    }
+
+}
