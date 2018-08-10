f361637c2042b8426646383d011de585c6cd14c8
Faiella <faiellaj@cubic.com>
2018-08-10 14:39:22 -0400

    Adding in playable classes

---
+++ Assets/Scripts/PartyMembers/Classes/Playable/Druid.cs
@@ -0,0 +1,20 @@
+﻿using HM_Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.PartyMembers.Classes.Playable
+{
+    public class Druid : CharacterData
+    {
+        //Class specific stuff?
+
+        // Use this for initialization
+        protected override void Start()
+        {
+            SetBaseStats("Druid", 100, 50, 100, 1.0f, 2.5f, CLASS_SPECIFIC_TYPE.DRUID);
+            SetResistances(.3f, .2f, .3f, .2f, .5f, .1f);
+        }
+    }
+}
---
+++ Assets/Scripts/PartyMembers/Classes/Playable/HolyAvenger.cs
@@ -0,0 +1,20 @@
+﻿using HM_Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.PartyMembers.Classes.Playable
+{
+    public class HolyAvenger : CharacterData
+    {
+        //Class specific stuff?
+
+        // Use this for initialization
+        protected override void Start()
+        {
+            SetBaseStats("Holy Avenger", 100, 50, 100, 1.0f, 2.5f, CLASS_SPECIFIC_TYPE.HOLY_AVENGER);
+            SetResistances(.3f, .2f, .3f, .2f, .5f, .1f);
+        }
+    }
+}
